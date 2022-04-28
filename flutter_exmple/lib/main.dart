2a05f81de9facdd76b54485c263a7308ecda201a
Oumar Fall <foumar404@gmail.com>
2022-04-28 02:28:12 +0100

    adding to profile and ui screen

--- flutter_exmple/lib/main.dart
+++ flutter_exmple/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_exmple/screen/home.dart';
 import 'package:flutter_exmple/widgets/Expanded.dart';
 import 'package:flutter_exmple/widgets/LlistView.dart';
 import 'package:flutter_exmple/widgets/Row.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ListViewExemple(),
+      home: HomePage(),
     );
   }
 }
